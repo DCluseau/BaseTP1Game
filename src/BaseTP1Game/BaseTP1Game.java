059ab83b37961a4afc77e2ea999ada72cbdede30
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-10-29 14:12:32 +0100

    Clean up code + comments added

--- src/BaseTP1Game/BaseTP1Game.java
+++ src/BaseTP1Game/BaseTP1Game.java
@@ -3,16 +3,26 @@ package BaseTP1Game;
 public class BaseTP1Game {
 
 	public static void main(String[] args) {
+		// Initialization of the game
 		Game newGame = new Game();
+		// Computer asks user if they want to play the game
 		newGame.userChoiceGame();
+		// If they accept, the game begins, else the application closes
 		while(newGame.continueGame) {
+			// Computer asks a number
 			newGame.enterNumber();
 			while(newGame.displayStep() == false) {
+				// While the player doesn't win, the game continues and displays if the number is higher or lower
 				newGame.enterNumber();
+				// If the player guesses right, the computer displays how many tries it took to win
 			}
+			// The computer asks the player if they want to continue the game
 			newGame.userChoiceGame();
 		}
+		// Closing the scanner
+		newGame.scan.close();
 		System.out.println("Goodbye !");
+		// End of program
 	}
 
 }
--- src/BaseTP1Game/Game.java
+++ src/BaseTP1Game/Game.java
@@ -3,18 +3,29 @@ package BaseTP1Game;
 import java.util.Scanner;
 
 public class Game {
-	int min = 0;
-	int max = 99;
-	int range = max - min + 1;
-	int nbRandom = (int) (Math.random() * range) + min;
-	String testChoice = new String();
-	int userChoice = -1;
-	Scanner scan = new Scanner(System.in);
-	int score = 1;
-	boolean continueGame = true;
-	String userContinue = "Y";
-	boolean containsYN = false;
+	// Variables initialization
+	// Minimum number to find - 1
+	int min;
+	// Maximum number to find - 1
+	int max;
+	// Range in which the number must be chosen
+	int range;
+	// Generated random number
+	int nbRandom;
+	// Number the player enters
+	int userChoice;
+	// Scanner to read the player's input
+	Scanner scan;
+	// Number of tries before winning (score)
+	int score;
+	// Indicates if the player wants to continue the game or not
+	boolean continueGame;
+	// String to verify if the string entered when continue game is asked
+	String userContinue;
+	// Boolean to verify if the player input Y or N when asked
+	boolean containsYN;
 	
+	// Contructor
 	public Game() {
 		this.min = 0;
 		this.max = 99;
@@ -29,15 +40,20 @@ public class Game {
 	}
 	
 	public void enterNumber() {
+		// Computer asks the player to input a number
 		System.out.println("Please enter a number between 1 and 100 : \n");
+		// Computer checks if a number has been entered
 		while(!scan.hasNextInt()) {
+			// Computer displays error if not
 			System.out.println("Error : you must enter a number between 1 and 100 : ");
 			scan.next();
 		}
+		// Computer saves the number entered
 		this.userChoice = this.scan.nextInt();
 	}
 	
 	public boolean displayStep() {
+		// Checks if the number is higher, lower or equal to the random number
 		if(this.userChoice < this.nbRandom) {
 			System.out.println("Please enter a higher number.");
 			this.score ++;
@@ -53,6 +69,7 @@ public class Game {
 	}
 	
 	public void userChoiceGame() {
+		// Computer asks if the player wants to play the game or if they want to continue
 		this.containsYN = false;
 		if(this.score == 1) {
 			System.out.println("Hello, would you like to play my game ? Y/N \n");
@@ -60,15 +77,21 @@ public class Game {
 			System.out.println("Would you like to replay the game ? Y/N \n");
 		}
 		while(!this.containsYN){
+			// Computer checks if Y or N has been entered
+			// Computer waits for a response from the player
 			this.userContinue = this.scan.next();
 			if(this.userContinue.equals("Y")) {
 				this.continueGame = true;
 				this.containsYN = true;
+				// Random number is generated
 				this.nbRandom = (int) (Math.random() * range) + min;
+				// Score is reinitialized
+				this.score = 1;
 			}else if(this.userContinue.equals("N")) {
 				this.continueGame = false;
 				this.containsYN = true;
 			}else {
+				// If Y or N haven't been entered, the computer displays an error and asks again a response
 				System.out.println("Please enter Y or N.\n");
 				this.containsYN = false;
 				this.scan.nextLine();
